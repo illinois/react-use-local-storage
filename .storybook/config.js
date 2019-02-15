1a8ea1dcdd1eb4eb396c0c732400f87ae0d03112
Nathan Walters <nwalters512@gmail.com>
2019-02-15 17:49:29 -0600

    Update build configuration

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,7 @@
 import { configure } from '@storybook/react';
 
 // automatically import all files ending in *.stories.js
-const req = require.context('../stories', true, /.(js|ts|tsx)$/);
+const req = require.context('../src', true, /.stories.(js|ts|tsx)$/);
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -2,7 +2,10 @@ module.exports = (baseConfig, env, config) => {
   config.module.rules.push({
     test: /\.(ts|tsx)$/,
     use: [{
-      loader: require.resolve('awesome-typescript-loader')
+      loader: require.resolve('awesome-typescript-loader'),
+      options: {
+        configFileName: './.storybook/tsconfig.json'
+      }
     }]
   });
   config.resolve.extensions.push('.ts', '.tsx');
